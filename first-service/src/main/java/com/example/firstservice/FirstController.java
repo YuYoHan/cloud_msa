005d59b92c4602fa325e08c9ee7e5df7a809c32f
YuYoHan <zxzz8014@naver.com>
2025-03-28 19:42:31 +0900

    [Fix] Cloud Gateway 리액티브 구성

--- first-service/src/main/java/com/example/firstservice/FirstController.java
+++ first-service/src/main/java/com/example/firstservice/FirstController.java
@@ -1,6 +1,7 @@
 package com.example.firstservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,10 @@ public class FirstController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to First project";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request")String header) {
+        return "Hello World in First message";
     }
 }
