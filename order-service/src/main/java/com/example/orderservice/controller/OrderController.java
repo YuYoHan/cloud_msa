b2ff288e9dca29278b54b0b2cfc2c9bfb1176469
YuYoHan <zxzz8014@naver.com>
2025-04-10 22:54:24 +0900

    [Add] order-service를 gateway에 등록

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,11 +1,17 @@
 package com.example.orderservice.controller;
 
+import com.example.orderservice.dto.OrderDTO;
+import com.example.orderservice.dto.RequestOrder;
+import com.example.orderservice.dto.ResponseOrder;
 import com.example.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +26,22 @@ public class OrderController {
                 env.getProperty("local.server.port"));
     }
 
-    
+    @PostMapping("/{userId}/orders")
+    public ResponseEntity<?> createOrder(@RequestBody RequestOrder order,
+                                         @PathVariable("userId")String userId) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        OrderDTO orderDTO = mapper.map(order, OrderDTO.class);
+        orderDTO.setUserId(userId);
+        ResponseOrder responseOrder = orderService.createdOrder(orderDTO);
+        return ResponseEntity.ok().body(responseOrder);
+    }
+
+    @GetMapping("/{userId}/orders")
+    public ResponseEntity<?> createOrder(@PathVariable("userId")String userId) {
+        List<ResponseOrder> response = orderService.getOrdersByUserId(userId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("")
 }
