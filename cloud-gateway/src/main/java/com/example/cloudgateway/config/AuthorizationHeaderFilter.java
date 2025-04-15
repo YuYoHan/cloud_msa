0f733fd5f8409a3117737921811c0b74a0e1da60
YuYoHan <zxzz8014@naver.com>
2025-04-15 13:39:01 +0900

    [Feat] gateway에 토큰 검증 기능 구현

---
+++ cloud-gateway/src/main/java/com/example/cloudgateway/config/AuthorizationHeaderFilter.java
@@ -0,0 +1,86 @@
+package com.example.cloudgateway.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+    private final Environment env;
+
+    public static class Config {
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authorizationHeader.replace("Bearer","");
+
+            if(!isJwtValid(jwt)) {
+                return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+            }
+
+            return chain.filter(exchange);
+        });
+    }
+
+    private boolean isJwtValid(String jwt) {
+        boolean returnValue = true;
+
+        String secret = env.getProperty("token.secret");
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
+
+        String subject = null;
+
+        try {
+            subject = Jwts.parser()
+                    .verifyWith(key)         // 서명 키 설정
+                    .build()
+                    .parseSignedClaims(jwt)  // 서명된 JWT 파싱
+                    .getPayload()
+                    .getSubject();           // 서브젝트 추출
+        } catch (Exception e) {
+            returnValue = false;
+        }
+
+        if(subject == null || subject.isEmpty()) {
+            returnValue = false;
+        }
+
+
+        return returnValue;
+    }
+
+    // Mono, Flux -> Spring WebFlux
+    private Mono<Void> onError(ServerWebExchange exchange,
+                               String err,
+                               HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+
+        log.error(err);
+        return response.setComplete();
+    }
+}
